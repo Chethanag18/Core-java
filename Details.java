7f9e7342f48228bc4d9cbf8aefea8593e5022ccb
Chethana <chethanag18.xworkz@gmail.com>
2024-07-19 10:46:31 +0530

    String array

---
+++ Details.java
@@ -0,0 +1,26 @@
+class Details
+{
+	public static void main(String[] args)
+	{
+		String email1="chethanag705@gmail.com";
+		String storess="bhagyashreehiremath7663@gmail.com";
+		String valuess="meghanag2000@gmail.com";
+		String details="chandanav@008@gmail.com";
+		String work="gadigeshvveerappa@gmail.com";
+		String laptop="anusha2000@gmail.com";
+		String mobile="xworkz@gmail.com";
+		String chair="chethanag18xworkz@gmail.com";
+		String ring="latha678@gmail.com";
+		String water="pushpa567@gmail.com";
+		String bangel="gana456@gamil.com";
+		String food="leo234@gamil.com";
+		String eat="rocky234@gmail.com";
+		String drink="druthi123@gmail.com";
+		String name="kumar456@gmail.com";
+		String[] email={email1,storess,valuess,details,work,laptop,mobile,chair,ring,water,bangel,food,eat,drink,name};
+		int store=email.length; // finding the total number of elements present in an array.
+		System.out.println("The total number of emails are:"+store);
+		String value=email[4]; // finding the element using the index number.
+		System.out.println("The email Id of Gadigesh is:"+value);
+	}
+}
