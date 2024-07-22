f8bb44dfa1d36360918e84c7b3c407b41faed49d
Chethana <chethanag18.xworkz@gmail.com>
2024-07-22 17:40:10 +0530

    arguments

---
+++ MedicalShop.java
@@ -0,0 +1,13 @@
+//wrapper classes
+//jar which contains the .class files.
+class MedicalShop
+{
+	public static void medicine(String name,String ManfDate,double price,int quantity)
+	{
+		System.out.println("Nmae:"+name);
+		System.out.println("Manufactue Date:"+ManfDate);
+		System.out.println("Price:"+price);
+		System.out.println("Quantity:"+quantity);
+		
+	}
+}
