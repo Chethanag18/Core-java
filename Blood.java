d4e38b87b764641d6fe5a5e6f07826246c30b20e
Chethana <chethanag18.xworkz@gmail.com>
2024-07-22 17:51:10 +0530

    java

---
+++ Blood.java
@@ -0,0 +1,33 @@
+class Blood
+{
+	public static String group()
+	{
+		String group="O +ve";
+		return group;
+	}
+	public static String personName()
+	{
+		String name="Chethana";
+		return name;
+	}
+	public static double cost()
+	{
+		double cost=2400;
+		return cost;
+	}
+	public static String hospitalTested()
+	{
+		String hospital="Narayan hospital";
+		return hospital;
+	}
+	public static boolean sickness()
+	{
+		boolean sickness=false;
+		return sickness;
+	}
+	public static boolean donate()
+	{
+		boolean donate=true;
+		return donate;
+	}
+}
