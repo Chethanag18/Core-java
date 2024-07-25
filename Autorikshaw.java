941a973bdfe5dbbec52c53b0da614a1ea41dec34
Chethana <chethanag18.xworkz@gmail.com>
2024-07-25 15:21:05 +0530

    java

---
+++ Autorikshaw.java
@@ -0,0 +1,10 @@
+class Autorikshaw
+{
+	double cost;
+	String color;
+	String company;
+	public Autorikshaw()
+	{
+		System.out.println("Created Autorikshaw");
+	}
+}
---
+++ AutorikshawRunner.java
@@ -0,0 +1,15 @@
+class AutorikshawRunner
+{
+	public static void main(String[] args)
+	{
+		Autorikshaw autorikshaw=new Autorikshaw();
+		double cost=autorikshaw.cost;
+		String color=autorikshaw.color;
+		String company=autorikshaw.company;
+		System.out.println("The cost of autorikshaw is "+cost);
+		System.out.println("The color of autorikshaw is "+color);
+		System.out.println("The company of autorikshaw is "+company);
+		
+		
+	}
+}
